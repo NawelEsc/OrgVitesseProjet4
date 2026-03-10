9f7d52e4e66ce6691251752085ff41d371637f7d
Nawel Escuder <nmescuder@outlook.fr>
2026-03-10 17:13:03 +0100

    modif OpportunityProducts.html et OpportunityProducts.js

--- force-app/main/default/lwc/opportunityProducts/opportunityProducts.js
+++ force-app/main/default/lwc/opportunityProducts/opportunityProducts.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
+import getOpportunityProducts from '@salesforce/apex/OpportunityProductsController.getOpportunityProducts';
 
-export default class OpportunityProducts extends LightningElement {}
+export default class OpportunityProducts extends LightningElement {
+    @api recordId;
+    @track products;
+
+    @wire(getOpportunityProducts, { opportunityId: '$recordId' })
+    wiredProducts({ error, data }) {
+        if (data) {
+            this.products = data;
+        } else if (error) {
+            console.error('Erreur Apex : ', error);
+        }
+    }
+}
